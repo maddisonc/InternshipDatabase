78d5323765aa2a9b69421d883396c6ea7e9e9c43
Maddison C <38960421+maddisonc@users.noreply.github.com>
2023-01-24 05:08:17 +0000

    1/23/23: 9:08pm - Added third sort for all internships by character length (length of position name) - sorts from longest to shortest and outputs length of name (number of chars), formatting

--- main.sql
+++ main.sql
@@ -16,7 +16,7 @@ INSERT INTO internships values
   (' Primary Care/Addiction Medicine Clinic Intern ', ' Coastal Primary Care ', ' Fullerton, CA ', false, null, ' https://rb.gy/q7sdtx'),
   (' Medical Scientist Training Program ', ' UC San Diego School of Medicine ', ' San Diego, CA ', true, null, ' https://rb.gy/cgnvvr'),
   (' Cell, Developmental & Cancer Biology Summer Research Internship ', ' Knight Cancer Institute ', ' Portland, Oregon ', true, null, ' https://rb.gy/e0ykpp'),
-  (' Immersive Pre-health Emergency and Wilderness Medicine Internship ', ' University of Colorado ', ' Aurora, CO ', true, null, ' https://rb.gy/w0g8dc'),
+  (' Immersive Pre-Health Emergency and Wilderness Medicine Internship ', ' University of Colorado ', ' Aurora, CO ', true, null, ' https://rb.gy/w0g8dc'),
   (' Ophthalmic Intern ', ' Century Eye Care ', ' Los Angeles, CA ', false, 16.00, ' https://rb.gy/fvyadp'),
   (' Summer Institute for Training in Biostatistics and Data Science at Columbia ', ' Columbia University Mailman School of Public Health ', ' New York, NY ', true, null, ' https://rb.gy/o8errg'),
   (' Medical Assistant in Family Medicine ', ' PIH Health Physicians ', ' Whittier, CA ', false, 17.00, ' https://rb.gy/mdhcqt'),
@@ -31,7 +31,7 @@ INSERT INTO internships values
 -- prints entire table in numbered list, no sorting
 -- tut: https://learnsql.com/cookbook/how-to-number-rows-in-sql/
 .print
-.print 'Query #1 - All Internships in Numbered List:'
+.print 'Query #1 - All internships in numbered list:'
 .print
 SELECT ROW_NUMBER() OVER() AS num_row,
 * FROM internships;
@@ -41,9 +41,9 @@ SELECT ROW_NUMBER() OVER() AS num_row,
 -- prints paid internships with rate, no sorting
 -- tut: https://stackoverflow.com/questions/29679551/sql-select-only-non-empty-columns
 .print
-.print 'Query #2 - Paid Internships:'
+.print 'Query #2 - Paid internships:'
 .print
-SELECT 'Pay Per Hour: ' AS ColumnName, payPerHr AS ColumnValue,
+SELECT 'Pay per hour: ' AS ColumnName, payPerHr AS ColumnValue,
 * FROM internships
 WHERE NOT payPerHr IS NULL;
 .print
@@ -52,7 +52,7 @@ WHERE NOT payPerHr IS NULL;
 -- prints internships with deadlines
 -- tut: https://stackoverflow.com/questions/29679551/sql-select-only-non-empty-columns
 .print
-.print 'Query #3 - Internships with Deadlines:'
+.print 'Query #3 - Internships with deadlines:'
 .print
 SELECT 'Has application deadline: ' AS ColumnName, deadline AS ColumnValue,
 * FROM internships
@@ -70,7 +70,7 @@ WHERE NOT deadline IS FALSE;
 -- alphabetical sort
 -- tut: https://learnsql.com/cookbook/how-to-order-alphabetically-in-sql/
 .print
-.print 'Sort #1 - Internships in Alphabetical Order by Position Name:'
+.print 'Sort #1 - Internships in alphabetical order by position name:'
 .print
 SELECT position,
 * FROM internships
@@ -80,9 +80,21 @@ ORDER BY position ASC;
 -- sort #2
 -- alphabetical sort
 .print
-.print 'Sort #2 - Internships in Alphabetical Order by City Name:'
+.print 'Sort #2 - Internships in alphabetical order by city name:'
 .print
 SELECT 'Location: ' AS ColumnName, location AS ColumnValue,
 * FROM internships
 ORDER BY location ASC; 
 .print
+
+-- sort #3
+-- length sort
+.print
+.print 'Sort #3 - Internships by length of position name: (longest to shortest)'
+.print
+SELECT 'Character length: ', LENGTH(position)-2, position,
+* FROM internships
+ORDER BY LENGTH(position) DESC;
+.print
+
+.print '---------------------------------------------------------------'
